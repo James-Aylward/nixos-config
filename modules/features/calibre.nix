99724671dca4d5af85970db21b9c7650030fc249
James Aylward <james.michael.aylward@gmail.com>
2026-06-29 14:32:28 +1000

    adds calibre module

---
+++ modules/features/calibre.nix
@@ -0,0 +1,13 @@
+{inputs, ...}: {
+  flake.nixosModules.calibre = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+    calibre
+    ];
+
+  networking.firewall = {
+    enable = true;
+    allowedUDPPorts = [ 54982 ];
+    allowedTCPPorts = [ 9090 ];
+  };
+  };
+}
